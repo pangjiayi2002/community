3c615e009ca4a4d98a5c331b9d2264c39ab4bd62
Jating <2301771566@qq.com>
2023-12-12 15:34:43 +0800

    commit

---
+++ restaurant1/src/main/java/servlet/manager/LogoutServlet.java
@@ -0,0 +1,21 @@
+package servlet.manager;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", value = "/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd=request.getRequestDispatcher("/adminLogin.jsp");
+        rd.forward(request,response);
+    }
+}
