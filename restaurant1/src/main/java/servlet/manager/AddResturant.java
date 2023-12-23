e786fee3bd050856367a40eebe6fc296d87813d8
Jating <2301771566@qq.com>
2023-12-23 12:19:25 +0800

    commit

--- restaurant1/src/main/java/servlet/manager/AddResturant.java
+++ restaurant1/src/main/java/servlet/manager/AddResturant.java
@@ -18,7 +18,7 @@ public class AddResturant extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int id= Integer.parseInt(request.getParameter("id"));
-        String username=request.getParameter("username");
+        String username=request.getParameter("name");
         String introduction=request.getParameter("introduction");
         String cover=request.getParameter("cover");
         String location=request.getParameter("location");
